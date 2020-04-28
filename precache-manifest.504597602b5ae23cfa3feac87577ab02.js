daff724d03c2bae66504b6197807c7b1c522005b
shahab <shahab.emami95@gmail.com>
2020-04-28 22:16:57 +0430

    Updates

---
+++ precache-manifest.504597602b5ae23cfa3feac87577ab02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dcd76c153143eecf77b9dc51a5fccad",
+    "url": "/chairs/index.html"
+  },
+  {
+    "revision": "dbfeed0effde88ae6402",
+    "url": "/chairs/static/css/main.2c496566.chunk.css"
+  },
+  {
+    "revision": "65398d08d5daef9948f7",
+    "url": "/chairs/static/js/2.737554ef.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/chairs/static/js/2.737554ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbfeed0effde88ae6402",
+    "url": "/chairs/static/js/main.e0b4f373.chunk.js"
+  },
+  {
+    "revision": "b1a22639dae777091168",
+    "url": "/chairs/static/js/runtime-main.d7434da2.js"
+  }
+]);
